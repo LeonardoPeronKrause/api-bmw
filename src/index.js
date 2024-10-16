6ad566046ef1d1147fb264372a32c0acddda3dc8
Leonardo Peron Krause <lperonkrause@gmail.com>
2024-10-16 12:35:42 -0300

    Continuando o projeto, parte 2 e 3

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const Car = mongoose.model('Car', {
 
 // Conectar com MongoDb
 app.listen(port, () => {
-    mongoose.connect('mongodb+srv://leonardo:SWGz601uUrTG0PzD@api-bmw.gbaaf.mongodb.net/?retryWrites=true&w=majority&appName=api-bmw')
+    mongoose.connect('mongodb+srv://leonardo:SWGz601uUrTG0PzD@api-bmw.gbaaf.mongodb.net/?retryWrites=true&w=majority&appName=api-bmw');
     console.log(`Aplicação rodando na porta ${port}`);
 });
 
@@ -26,19 +26,67 @@ app.get('/', async (req, res) => {
     const cars = await Car.find();
     return res.send(cars);
     } catch (error) {
-        res.status(500).send({error: 'Erro ao buscar carros.'})
+        res.status(500).send({error: 'Erro ao buscar carros.'});
     }
 });
 
+app.get('/data/average-speed', async (req, res) => {
+    try {
+        // Busca todosos carros no db
+        const cars = await Car.find();
+
+        // Verifica se existe dados de veiculos no db
+        if (cars.length === 0) {
+            return res.send({averageSpeed: 0}); // Se não houver carros, a média é 0
+        }
+
+        // Criar um array com as velocidades dos carros
+        const speeds = cars.map(car => car.speed);
+
+        // Inicializa a variavel
+        let totalSpeed = 0;
+
+        //  Usar forEach para somar as velocidades
+        speeds.forEach(speed => {
+            totalSpeed = totalSpeed + speed; 
+        });
+
+        // Calcular a média de velocidade
+        const averageSpeed = totalSpeed / speeds.length;
+
+        // Retorna a média de velocidade
+        return res.send({ averageSpeed });
+    } catch (error) {
+        res.status(500).send({error: 'Erro ao calcular a média de velocidade.'});
+    }
+})
+
 // Rota para adicionar um novo carro
 app.post('/data', async (req, res) => {
+    const { vehicleModel, speed, engineTemperature, fuelLevel} = req.body;
 
-    // Criar uma nova instância do modelo Car com os dados recebidos 
+    if (vehicleModel === '') {
+        return res.status(400).send({error: 'O modelo do veiculo deve ser válido.'});
+    }
+
+    if (typeof speed !== 'number' || speed <= 0) {
+        return res.status(400).send({error: 'A velocidade não pode ser negativa.'});
+    }
+
+    if (engineTemperature > 200 || engineTemperature < -30) {
+        return res.status(400).send({error: 'Temperatura do motor inválida.'});
+    }
+
+    if (fuelLevel < 0 || fuelLevel > 100) {
+        return res.status(400).send({error: 'O nível de combutível deve ser entre 0 e 100'});
+    }
+
+    // Criar uma nova instância do modelo Car com os dados validados 
     const car = new Car({
-        vehicleModel: req.body.vehicleModel,
-        speed: req.body.speed,
-        engineTemperature: req.body.engineTemperature,
-        fuelLevel: req.body.fuelLevel
+        vehicleModel,
+        speed,
+        engineTemperature,
+        fuelLevel
     });
 
     try {
@@ -46,6 +94,6 @@ app.post('/data', async (req, res) => {
         await car.save()
         return res.status(201).send(car);
     } catch (error) {
-        return res.status(500).send({error: 'Erro ao salvar o carro no banco de dados.'})
+        return res.status(500).send({error: 'Erro ao salvar o carro no banco de dados.'});
     }
 });
